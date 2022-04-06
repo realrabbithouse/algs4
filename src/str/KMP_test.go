3b6db7a5b0a4905946d6297e78a2d4bbf44a59f5
Yin Bangtao <1286816442@qq.com>
2022-04-06 20:41:39 +0800

    Rename file.

--- src/str/KMP_test.go
+++ src/str/KMP_test.go
@@ -9,4 +9,7 @@ func TestKMP(t *testing.T) {
 	kmp := NewKMP("AACAA")
 	n := kmp.Search("AABRAACADABRAACAADABRA")
 	fmt.Println("target hit at", n)
+
+	n = kmp.Search("when rrr rabbit rules the world, a lot of rrr rabbits...")
+	fmt.Println("target hit at", n)
 }
